495ac184d6dee69aed299eca6fc08f233f6a67e7
xSankari <xSankari@gmail.com>
2022-01-06 19:13:10 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import time
+import json
+from tkinter import *
+
+
+def update():
+    canvas.delete('all')
+    var_t = time.strftime(time_format)
+    x_val = root.winfo_width() / 2
+    y_val = root.winfo_height() / 2
+    canvas.create_text(x_val, y_val, text=var_t, font=(font, 55), fill=text_color)
+    canvas.after(100, update)
+
+
+try:
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+except FileNotFoundError:
+    with open('config.json', 'w') as f:
+        f.write('{\n')
+        f.write('  "bg_color" : "black",\n')
+        f.write('  "text_color" : "white",\n')
+        f.write('  "font" : "comic sans ms",\n')
+        f.write('  "time_format" : "%I:%M:%S"\n')
+        f.write('}')
+        raise FileNotFoundError('Error. Restart.')
+
+
+bg_color = config['bg_color']
+text_color = config['text_color']
+font = config['font']
+time_format = config['time_format']
+
+root = Tk()
+root.title('Window Title')
+canvas = Canvas(root, bg=bg_color)
+canvas.pack(fill='both', expand=YES)
+
+
+update()
+root.mainloop()
